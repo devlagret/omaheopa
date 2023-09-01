87b6c1cdb7eb0469b9e5dea145df2ab3723443d8
devlagret <sioputra4@gmail.com>
2023-09-01 17:05:09 +0700

    Add relation

--- app/Models/InvtItemStock.php
+++ app/Models/InvtItemStock.php
@@ -10,6 +10,18 @@ class InvtItemStock extends Model
     // use HasFactory;
     protected $table        = 'invt_item_stock';
     protected $primaryKey   = 'item_stock_id';
+    public function item() {
+        return $this->belongsTo(InvtItem::class,'item_id','item_id');
+    }
+    public function unit() {
+        return $this->belongsTo(InvtItemUnit::class,'item_unit_id','item_unit_id');
+    }
+    public function category() {
+        return $this->belongsTo(InvtItemCategory::class,'item_category_id','item_category_id');
+    }
+    public function warehouse() {
+        return $this->belongsTo(InvtWarehouse::class,'warehouse_id','warehouse_id');
+    }
     protected $guarded = [
         'updated_at',
         'created_at'
