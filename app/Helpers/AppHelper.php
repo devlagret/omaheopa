d022251e9c04167c89502c6181cabf5268b50b46
devlagret <sioputra4@gmail.com>
2023-09-15 17:06:57 +0700

    Update Helper

--- app/Helpers/AppHelper.php
+++ app/Helpers/AppHelper.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Helpers;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Storage;
 
 class AppHelper
@@ -8,5 +9,11 @@ class AppHelper
     {
         $quotes = collect(json_decode(Storage::get('public/quotes.min.json')))->random();
         return $quotes->quote.' - '.$quotes->by;
+    }
+    public static function status():Collection {
+        return collect(['id'=>[1=>'Sudah Dibayar',2=>'Sudah Check-In',3=>'Sudah Check-Out',],'type'=>[1=>'success',2=>'primary',3=>'info',]]);
+    }
+    public static function menuType():Collection {
+        return collect([1 => 'Breakfast', 2 => 'Lunch', 3 => 'Dinner']);
     }
 }
