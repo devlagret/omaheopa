44875c40da62f3766c8ee4a2f372448e24847dfb
sayyidazizii <sayyidsyafiq234@gmail.com>
2023-09-26 16:51:56 +0700

    update Downpayment & AcctAset

--- app/Http/Controllers/AcctAssetController.php
+++ app/Http/Controllers/AcctAssetController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\AcctAsset;
+use App\Models\AcctAssetReport;
+use App\Models\AcctAssetReportItem;
 use App\Models\AcctAssetType;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -48,35 +50,49 @@ class AcctAssetController extends Controller
 
 
     public function depreciation($asset_id){
-        $acctasset  = AcctAsset::find($asset_id);
-        
-        return view('content.AcctAset.FormDetailAcctAssetDepreciation',compact('acctasset'));
+        $acctasset  = AcctAssetReport::select('*')
+        ->join('acct_asset', 'acct_asset_depreciation.asset_id','acct_asset.asset_id')
+        ->where('acct_asset_depreciation.asset_id',$asset_id)
+        ->first();
+        $acctassetItem = $this->getAcctAssetDepreciationItem_Detail($asset_id);
+        $account    = $this->getAcctAccount($asset_id);
+
+        // dd($acctasset,$acctassetItem,$account);
+        return view('content.AcctAset.FormDetailAcctAssetDepreciation',compact('acctasset','acctassetItem','account'));
     }
 
-    // public function getAcctAssetDepreciationItem_Detail($id){
-    //     $data = AcctAsset::  select('acct_asset_depreciation_item.asset_depreciation_item_id, acct_asset_depreciation_item.asset_depreciation_item_year_to, acct_asset_depreciation_item.asset_depreciation_item_month, acct_asset_depreciation_item.asset_depreciation_item_year, acct_asset_depreciation_item.asset_depreciation_item_amount, acct_asset_depreciation_item.asset_depreciation_item_book_value, acct_asset_depreciation_item.asset_depreciation_item_accumulation_amount, acct_asset_depreciation_item.asset_depreciation_item_journal_status, acct_asset_depreciation_item.asset_depreciation_item_journal_date');
-    //     $this->database->from('acct_asset_depreciation_item');
-    //     $this->database->join('acct_asset_depreciation', 'acct_asset_depreciation_item.asset_depreciation_id = acct_asset_depreciation.asset_depreciation_id');
-    //     $this->database->join('acct_asset', 'acct_asset_depreciation.asset_id = acct_asset.asset_id');
-    //     $this->database->where('acct_asset_depreciation.asset_id',$id);
-    //     return $this->database->get()->result_array();
-    // }
+    public function getAcctAssetDepreciationItem_Detail($asset_id){
+        $data = AcctAssetReport::select('*')
+        ->join('acct_asset_depreciation_item', 'acct_asset_depreciation_item.asset_depreciation_id','acct_asset_depreciation.asset_depreciation_id')
+        ->join('acct_asset', 'acct_asset_depreciation.asset_id','acct_asset.asset_id')
+        ->where('acct_asset_depreciation.asset_id',$asset_id)
+        ->get();
 
-    // public function getAcctAsset_Detail($id){
-    //     $this->database->select('acct_asset.asset_id, acct_asset.asset_type_id, acct_asset_type.asset_type_name, acct_asset.location_id, acct_asset.asset_purchase_date, acct_asset.asset_purchase_value, acct_asset.asset_depreciation_value, acct_asset.asset_location_detail, acct_asset.asset_description, acct_asset.asset_code, acct_asset.asset_name, acct_asset.voided_remark, acct_asset.asset_book_value, acct_asset.asset_depreciation_type, acct_asset.asset_salvage_value, acct_asset.asset_estimated_lifespan, acct_asset.item_unit_code');
-    //     $this->database->from('acct_asset');
-    //     // $this->database->join('core_location', 'acct_asset.location_id = core_location.location_id');
-    //     $this->database->join('acct_asset_type', 'acct_asset.asset_type_id = acct_asset_type.asset_type_id');
-    //     $this->database->where('acct_asset.asset_id',$id);
-    //     $result = $this->database->get()->row_array();
-    //     return $result;
-    // }
+        return $data;
+    }
 
-    // public function getAcctAccount(){
-    //     $query = $this->database->query("SELECT account_id, concat(account_code,' - ',account_name) AS account_code FROM acct_account WHERE data_state = 0 AND parent_account_status = 0");
-    //     $result = $query->result_array();	
-    //     return $result;
-    // }
+    public function getAcctAsset_Detail($asset_id){
+        $data = AcctAssetReport::select('acct_asset.asset_id, acct_asset.asset_type_id, acct_asset_type.asset_type_name, acct_asset.location_id, acct_asset.asset_purchase_date, acct_asset.asset_purchase_value, acct_asset.asset_depreciation_value, acct_asset.asset_location_detail, acct_asset.asset_description, acct_asset.asset_code, acct_asset.asset_name, acct_asset.voided_remark, acct_asset.asset_book_value, acct_asset.asset_depreciation_type, acct_asset.asset_salvage_value, acct_asset.asset_estimated_lifespan, acct_asset.item_unit_code')
+        ->join('acct_asset_type', 'acct_asset.asset_type_id = acct_asset_type.asset_type_id')
+        ->where('acct_asset.asset_id',$asset_id);
+        return $data;
+    }
 
+    public function getAcctAccount(){
+        $query = DB::select("SELECT account_id, concat(account_code,' - ',account_name) AS account_code FROM acct_account WHERE data_state = 0 AND parent_account_status = 0");	
+        return $query;
+    }
 
+    public function DepreciationMethod($asset_type_id){
+		
+        $depreciation_method = 'Garis Lurus';
+        if($asset_type_id == 1){
+            $data = $depreciation_method;
+        }else{
+            $data = $depreciation_method;
+        }
+        
+
+		return $data;
+	}	
 }
