c756798c97e7b7fca192e8c36a18a5f8fb595821
devlagret <sioputra4@gmail.com>
2023-09-27 10:42:24 +0700

    add manifest

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="manifest" href="{{asset('manifest.json')}}" />
     <link rel="icon" type="image/x-icon"
         href="{{ asset('resources/assets/img/logo_baru_mozaic/logo baru set-09.png') }}" />
 
