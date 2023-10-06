550508333aa53a18a97f0400b008cf570656411e
sayyidazizii <sayyidsyafiq234@gmail.com>
2023-10-06 16:08:07 +0700

    update sayyid 06-10-23

--- app/Models/InvtItemMutation.php
+++ app/Models/InvtItemMutation.php
@@ -14,6 +14,15 @@ class InvtItemMutation extends Model
         'updated_at',
         'created_at'
     ];
+    public function item() {
+        return $this->belongsTo(InvtItem::class,'item_id','item_id');
+    }
+    public function category() {
+        return $this->belongsTo(InvtItemCategory::class,'item_category_id','item_category_id');
+    }
+    public function unit() {
+        return $this->belongsTo(InvtItemUnit::class,'item_unit_id','item_unit_id');
+    }
     protected static function booted()
     {
         static::addGlobalScope(new NotDeletedScope);
