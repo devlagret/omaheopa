62f624f2ee043b67d540308a6ec2f9ff2090cbe8
Vinsmoke-Tech <daffahanaris@gmail.com>
2023-11-21 17:23:28 +0700

    api

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -664,7 +664,7 @@ class APIController extends Controller
     {
         $data = PreferenceTransactionModule::where('transaction_module_code',$transaction_module_code)->first();
 
-        return $data['transaction_module_id'];
+        return $data['transaction_module_id']?? '';
     }
 
     public function getTransactionModuleName($transaction_module_code)
@@ -1939,16 +1939,18 @@ class APIController extends Controller
 
     public function postSalesTiket(Request $request)
     {
+        // Helper::sales(item_id,qty,unit,dis%)->withJournal();
         // dd($request->all());
         $transaction_module_code = 'SI';
         $transaction_module_id  = $this->getTransactionModuleID($transaction_module_code);
         $fields = $request->validate([
             'sales_invoice_date'        => 'required',
-            'subtotal_item'             => 'required',
-            'subtotal_amount1'          => 'required',
+            'subtotal_item'             => '',
+            'subtotal_amount1'          => '',
             'total_amount'              => 'required',
             'paid_amount'               => 'required',
-            'change_amount'             => 'required'
+            'item_id'                   => 'required',
+            'change_amount'             => ''
         ]);
         if (empty($request->discount_percentage_total)){
             $discount_percentage_total = 0;
@@ -1991,49 +1993,33 @@ class APIController extends Controller
 
         if(SalesInvoice::create($data)){
             // if(SalesInvoice::create($data)){
-            $sales_invoice_id   = SalesInvoice::orderBy('created_at','DESC')->where('company_id', Auth::user()->company_id)->first();
-            $arraydatases       = Session::get('arraydatases');
-            foreach ($arraydatases as $key => $val) {
+            $sales_invoice_id   = SalesInvoice::orderBy('created_at','DESC')->where('sales_status',1)->where('company_id', Auth::user()->company_id)->first();
+            $item = InvtItem::where('item_id', $fields['item_id'])->where('item_status',1)->first();
+
                 $dataarray = array(
                     'sales_invoice_id'                  => $sales_invoice_id['sales_invoice_id'],
-                    // 'item_category_id'                  => 13, 
-                    'item_unit_id'                      => $val['item_unit_id'],
-                    'item_id'                           => $val['item_id'],
-                    'quantity'                          => $val['quantity'],
-                    'item_unit_price'                   => $val['item_unit_price'],
-                    'subtotal_amount'                   => $val['subtotal_amount'],
-                    'discount_percentage'               => $val['discount_percentage'],
-                    'discount_amount'                   => $val['discount_amount'],
-                    'subtotal_amount_after_discount'    => $val['subtotal_amount_after_discount'],
+                    'item_category_id'                  => null, 
+                    'item_unit_id'                      => $item['item_unit_id'],
+                    'item_id'                           => $fields['item_id'],
+                    'quantity'                          => $item['quantity'],
+                    'item_unit_price'                   => $item['item_unit_price'],
+                    'subtotal_amount'                   => $item['subtotal_amount'],
+                    'discount_percentage'               => $item['discount_percentage'],
+                    'discount_amount'                   => $item['discount_amount'],
+                    'subtotal_amount_after_discount'    => $item['subtotal_amount_after_discount'],
                     'company_id'                        => Auth::user()->company_id,
                     'created_id'                        => Auth::id(),
                     'updated_id'                        => Auth::id()
                 );
-                // dump([(int) $val['quantity'],$val['item_unit_id']]);
-                // $qty = (int) $val['quantity'];
-                // dump(StockHelper::find($val['item_id']));
-                // dump(StockHelper::find(3)->sub(5,$val['item_unit_id']));
-                // dump(StockHelper::find($val['item_id']));
-                // dd($dataarray); 
-                StockHelper::find($val['item_id'])->sub((int)$val['quantity'],$val['item_unit_id']);
-                SalesInvoiceItem::create($dataarray);
-                // $stock_item = InvtItemStock::where('item_id',$dataarray['item_id'])
-                // ->where('item_category_id',$dataarray['item_category_id'])
-                // ->where('item_unit_id', $dataarray['item_unit_id'])
-                // ->where('company_id', Auth::user()->company_id)
-                // ->first();
-                // if(isset($stock_item)){
-                //     $table = InvtItemStock::findOrFail($stock_item['item_stock_id']);
-                //     $table->last_balance = $stock_item['last_balance'] - $dataarray['quantity'];
-                //     $table->updated_id = Auth::id();
-                //     $table->save();
+            if(SalesInvoiceItem::create($dataarray)){
 
-                // }
-                //stock update
+                StockHelper::find($dataarray['item_id'])->sub((int)$dataarray['quantity'],$dataarray['item_unit_id']);
+            }else{
+                return response([
+                    'message' => 'Data Tidak Berhasil Disimpan'
+                ],401);  
             }
 
-
-            
 
             // $account_setting_name = 'sales_cash_account';
             // $account_id = $this->getAccountId($account_setting_name);
@@ -2087,11 +2073,17 @@ class APIController extends Controller
             // );
             // JournalVoucherItem::create($journal_credit);
 
-            $msg = 'Tambah Tiket Penjualan Berhasil';
-            return redirect('/sales-tiket/add')->with('msg',$msg);
-        } else {
-            $msg = 'Tambah Tiket Penjualan Gagal';
-            return redirect('/sales-tiket/add')->with('msg',$msg);
+            // $msg = 'Tambah Tiket Penjualan Berhasil';
+            // return redirect('/sales-tiket/add')->with('msg',$msg);
+        } else{
+            return response([
+                'message' => 'Data Tidak Berhasil Disimpan'
+            ],401);            
         }
+        
+
+        return response([
+            'message' => 'Data Berhasil Disimpan'
+        ],201);
     }
 }
