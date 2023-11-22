348a261679558d0f71e9128d35aee34529e9ee8a
Vinsmoke-Tech <daffahanaris@gmail.com>
2023-11-22 15:43:59 +0700

    fix

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -1982,7 +1982,7 @@ class APIController extends Controller
         ->where('data_state', 0)
         ->where('company_id', Auth::user()->company_id)
         ->where('item_status',1)
-        ->orderBy('item_name', 'ASC')
+        ->orderBy('item_id', 'ASC')
         ->get();
         
         if($items){
