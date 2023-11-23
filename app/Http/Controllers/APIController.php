bcbf0b471201e28d54eb35a0628a965ba4d90a13
Vinsmoke-Tech <daffahanaris@gmail.com>
2023-11-23 14:41:43 +0700

    fix

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -1979,7 +1979,7 @@ class APIController extends Controller
     {   
         $fields = $request->validate([
             'user_id'           => 'required',
-            // 'item_id'           => 'required',
+            'item_id'           => 'required',
 
         ]);
 
@@ -1990,7 +1990,7 @@ class APIController extends Controller
 
         $items  = InvtItem::select('item_id','item_name','item_unit_price1')
         ->where('data_state', 0)
-        // ->where('item_id', $fields['item_id'])
+        ->where('item_id', $fields['item_id'])
         // ->where('item_name', $fields['item_name'])
         ->where('item_status',1)
         // ->orderBy('item_id', 'ASC')
