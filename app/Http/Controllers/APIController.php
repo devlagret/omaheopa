f94b3998fb8bc4743e3fb64360ee5900435c1bb8
Vinsmoke-Tech <daffahanaris@gmail.com>
2023-11-27 09:58:12 +0700

    da

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -1936,7 +1936,7 @@ class APIController extends Controller
 
     public function getSalesTiketMerchant()
     {
-        $merchant   = SalesMerchant::where('data_state', 0);
+        $merchant   = InvtItem::where('data_state', 0);
         if(Auth::id()!=1||Auth::user()->merchant_id!=null){
             $merchant->where('merchant_id',Auth::user()->merchant_id);
         }
