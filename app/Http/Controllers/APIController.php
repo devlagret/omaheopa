b7470a22355a0300f1939dc94dc5db68b006012a
Vinsmoke-Tech <daffahanaris@gmail.com>
2023-11-27 10:28:39 +0700

    da

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -1957,12 +1957,10 @@ class APIController extends Controller
         }
         $item = $item->get();
 
-        $units   = InvtItemUnit::where('data_state', 0)
-        ->where('company_id', Auth::user()->company_id)
-        ->get();
-
+        $item = InvtItem::find($request->item_id);
         $units          = InvtItemUnit::where('data_state', 0)
         ->where('company_id', Auth::user()->company_id)
+        ->where('item_unit_id',$item['item_unit_id'])
         ->get()
         ->pluck('item_unit_name','item_unit_id');
 
